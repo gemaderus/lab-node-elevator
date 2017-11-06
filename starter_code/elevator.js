8313174d15a46f693a371f32be8b01313f8bf80f
gemaderus <hi@gemaderus.com>
2017-11-06 17:53:54 +0100

    First iteration finished.

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -3,17 +3,36 @@ class Elevator {
     this.floor      = 0;
     this.MAXFLOOR   = 10;
     this.requests   = [];
+    this.direction = "up";
+    this.interval = 0;
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  start() {
+    this.interval = setInterval(()=> this.update(), 1000);
+
+  }
+
+  stop() {
+    clearInterval(this.interval);
+  }
+
+  update() {
+    this.log();
+  }
+  _passengersEnter() {
+  }
+  _passengersLeave() {
+  }
+  floorUp() {
+
+  }
+  floorDown() {
+  }
+  call() {
+  }
+  log() {
+    console.log("Direction: " + this.direction + " Floor: " + this.floor);
+  }
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,7 @@
 const Elevator = require('./elevator.js');
+const Person = require('./person.js');
+
+let elevator = new Elevator ();
+elevator.update();
+elevator.start();
+setTimeout(() => elevator.stop(), 4000);
